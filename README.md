b98dde1d06bdc6e9858bca394963793ac9fe36e4
Sietse de Kaper <sietse@lunatech.com>
2013-06-14 22:25:57 +0200

    Chapter 7 sample code

---
+++ README.md
@@ -0,0 +1,4 @@
+Chapter 7: Models
+=================
+
+This is the sample code for the first part of chapter seven of [Play for Java](http://bit.ly/playjava).
---
+++ app/models/StockItem.java
@@ -0,0 +1,26 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class StockItem extends Model{
+
+  @Id
+  public Long id;
+
+  @ManyToOne
+  public Warehouse warehouse;
+
+  @ManyToOne
+  public Product product;
+  public Long quantity;
+
+  @Override
+  public String toString() {
+    return String.format("%d %s", quantity, product);
+  }
+}
